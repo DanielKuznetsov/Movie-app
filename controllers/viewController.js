764d69e0b134929e7697cb6ca4a346abaddfd352
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-08-14 22:08:42 -0700

    Routes and pug pages have been implemented with data of liked, bookmarked, movies etc that are rendered on a server from a database

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -22,20 +22,37 @@ exports.getSignupPage = function (req, res, next) {
   });
 };
 
-exports.getMoviesPage = function (req, res, next) {
+exports.getMoviesPage = async function (req, res, next) {
+  const movies = await Movie.find({ category: "Movie" }).populate({
+    path: "whoLiked whoBookmarked",
+    select: "-__v",
+  });
+
   res.status(200).render("movies", {
     title: "All Movies Page",
+    movies,
   });
 };
 
-exports.getTVPage = function (req, res, next) {
+exports.getTVPage = async function (req, res, next) {
+  const allTVSeries = await Movie.find({
+    // '$options: i' field is neccessary because we need to specify case insensiity
+    category: { $regex: "tv s", $options: "i" },
+  });
+
   res.status(200).render("tv", {
     title: "TV Series Page",
+    allTVSeries,
   });
 };
 
 exports.getBookmarkedPage = function (req, res, next) {
+  const tvs = req.user.bookmarked.filter((el) => el.category === "TV Series");
+  const movies = req.user.bookmarked.filter((el) => el.category === "Movie");
+
   res.status(200).render("bookmarked", {
     title: "Bookmarked Page",
+    tvs,
+    movies,
   });
 };
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -7,8 +7,12 @@ const router = express.Router();
 router.get("/", authController.protect, viewController.getHomepage);
 router.get("/login", viewController.getLoginPage);
 router.get("/signup", viewController.getSignupPage);
-// router.get("/movies", authController.protect, viewController.getMoviesPage);
-// router.get("/tv", authController.protect, viewController.getTVPage);
-// router.get("/bookmarked", authController.protect, viewController.getBookmarkedPage);
+router.get("/movies", authController.protect, viewController.getMoviesPage);
+router.get("/tv", authController.protect, viewController.getTVPage);
+router.get(
+  "/bookmarked",
+  authController.protect,
+  viewController.getBookmarkedPage
+);
 
 module.exports = router;
