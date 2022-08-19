ee301475dcb0b406c73cd5fc90222debb8bf8144
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-08-19 13:40:27 -0700

    updated package.json()

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const dotenv = require("dotenv");
 const app = require("./app");
 const mongoose = require("mongoose");
+// const cloudinary = require("cloudinary");
+// const Movie = require("./models/movieModel");
 
 dotenv.config({ path: "./config.env" });
+
+// cloudinary.config({
+//   cloud_name: "dfgpplsw7",
+//   api_key: "977596339658681",
+//   api_secret: "IiKEoW7_Mv45pVom-w3Ai3rveMI",
+// });
+
+// const update = async (req, res, next) => {
+//   await Movie.findOneAndUpdate();
+// };
+
+// const image = cloudinary.image(
+//   "./starter-code/assets/thumbnails/1998/large.jpg"
+// );
+// console.log(image);
 
 const DB = process.env.DATABASE.replace(
   `<PASSWORD>`,
